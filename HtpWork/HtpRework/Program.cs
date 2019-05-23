b646c5ef62208f161afeec5cf2f00a1f2df5117d
AlexiMinor <aleximinor1310@gmail.com>
2019-05-23 20:11:12 +0300

    Do all homework

--- HtpWork/HtpRework/Program.cs
+++ HtpWork/HtpRework/Program.cs
@@ -6,7 +6,27 @@ namespace HtpRework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Input number: ");
+            var x = Console.ReadLine();
+            
+            Console.WriteLine($"You input {GetDayOfWeekByNumber(x)}");
+
+        }
+
+        static string GetDayOfWeekByNumber(string numberOfDay)
+        {
+            switch (numberOfDay)
+            {
+                case "1":
+                    return "Monday";
+                
+                case "2":
+                    return "Tuesday";
+                
+                default:
+                    return "Incorrect value for day of week";
+            }
+            
         }
     }
 }
--- HtpWork/HtpWork/Program.cs
+++ HtpWork/HtpWork/Program.cs
@@ -6,10 +6,36 @@ namespace HtpWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Input number: ");
+            Console.WriteLine("Input number:");
             var x = Console.ReadLine();
+
+            int number = Convert.ToInt32(x);
             
-            Console.WriteLine(x);
+            WriteArrayOfLongValues(Multiple(number), number);
+
+            Console.ReadLine();
+
         }
+
+        private static long[] Multiple(int number)
+        {
+            long[] array = new long[11];
+            for (int i = 0; i < 20; i++)
+            {
+                array[i] = number * i;
+            }
+
+            return array;
+        }
+
+        static void WriteArrayOfLongValues(long[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+               
+                    Console.WriteLine($"{number.ToString()} * {i.ToString()} = {array[i].ToString()}");
+               }
+            }
+        }
+
     }
-}
